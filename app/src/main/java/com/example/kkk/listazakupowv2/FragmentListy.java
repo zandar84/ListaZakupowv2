0c8debbf3ed7336f56d7872a787a2fb11da8ba33
zandar84 <zandar84@interia.pl>
2019-01-09 19:00:05 +0100

    działają 3 karty dodana biblioteka dla recyclerview

---
+++ app/src/main/java/com/example/kkk/listazakupowv2/FragmentListy.java
@@ -0,0 +1,24 @@
+package com.example.kkk.listazakupowv2;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentListy extends Fragment {
+
+    View v;
+
+    public FragmentListy() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.listy_fragment, container, false);
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/kkk/listazakupowv2/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.kkk.listazakupowv2;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        // Add fragment here
+        adapter.AddFragment(new FragmentListy(), "Listy");
+        adapter.AddFragment(new FragmentArtykuly(), "Artykuły");
+        adapter.AddFragment(new FragmentUstawienia(), "Ustawienia");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_assignment);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_shopping_cart);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_settings_applications);
+
+        // Remove shadow from the Action Bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setElevation(0);
+    }
+}
